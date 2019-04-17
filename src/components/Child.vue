db804332b20fc98bd644867a67a654c73d6ff19f
calarence284 <chenwenbiao284@163.com>
2019-04-17 22:09:18 +0800

    父子组件通信方式测试

---
+++ src/components/Child.vue
@@ -0,0 +1,33 @@
+<!--子组件获取父组件传递参数、调用父组件方法-->
+<template>
+    <div>
+      <h1>子组件</h1>
+      <button @click="sendMessageToParent">向父组件传值</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data () {
+    return {
+      msg: '子组件'
+    }
+  },
+  props: ['parentArgs'],
+  methods: {
+    callChild () {
+      console.log('parent invoke child')
+      alert(this.parentArgs)
+    },
+    sendMessageToParent () {
+      this.$emit('listenToChildEvent', '这是子组件传递的消息')
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,35 @@
+<!--父组件向子组件传参、调用子组件方法-->
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+      <button @click="parentInvokeChild" >父组件调用子组件</button>
+      <Child ref="mychild" parent-args="hhh" @listenToChildEvent="showMsgFromChild"></Child>
+    </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+  name: 'Parent',
+  data () {
+    return {
+      msg: '父组件'
+    }
+  },
+  components: {
+    Child
+  },
+  methods: {
+    parentInvokeChild () {
+      this.$refs.mychild.callChild(this.msg)
+    },
+    showMsgFromChild (data) {
+      alert(data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
